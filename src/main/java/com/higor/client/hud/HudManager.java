5ee2b3392b19258340806661c9ba90f81e562e5e
higor525 <gomeshigor033@gmail.com>
2026-09-12 17:35:54 -0300

    Create HudManager.java

---
+++ src/main/java/com/higor/client/hud/HudManager.java
@@ -0,0 +1,10 @@
+package com.higor.client.hud;
+
+public class HudManager {
+
+    public HudManager() {
+        System.out.println("[HIGOR CLIENT] HudManager inicializado (placeholder).");
+    }
+
+    // HUD Editor sera implementado na Etapa 4
+}
