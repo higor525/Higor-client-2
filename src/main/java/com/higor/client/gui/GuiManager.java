cc7997958acc2aecb2802b7939dca3f505a6444a
higor525 <gomeshigor033@gmail.com>
2026-09-12 17:35:20 -0300

    Create GuiManager.java

---
+++ src/main/java/com/higor/client/gui/GuiManager.java
@@ -0,0 +1,10 @@
+package com.higor.client.gui;
+
+public class GuiManager {
+
+    public GuiManager() {
+        System.out.println("[HIGOR CLIENT] GuiManager inicializado (placeholder).");
+    }
+
+    // GUI sera implementada na Etapa 3 (ClickGUI)
+}
