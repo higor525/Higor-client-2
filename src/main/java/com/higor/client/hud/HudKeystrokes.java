b6246241b4c9cd9d820205f242be85c4cef57475
higor525 <gomeshigor033@gmail.com>
2026-09-12 21:12:51 -0300

    Atualizar o HudKeystrokes.java

--- src/main/java/com/higor/client/hud/HudKeystrokes.java
+++ src/main/java/com/higor/client/hud/HudKeystrokes.java
@@ -1,8 +1,10 @@
 package com.higor.client.hud;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 public class HudKeystrokes extends HudModule {
 
@@ -20,34 +22,34 @@ public class HudKeystrokes extends HudModule {
 
         int bg = getBgColor();
         int textColor = getTextColor();
-        int boxSize = 16;
+        int box = 16;
         int gap = 2;
 
         // W
         drawKey(mc, "W", Keyboard.isKeyDown(Keyboard.KEY_W),
-                0, 0, boxSize, bg, textColor);
+                0, 0, box, bg, textColor);
 
         // A
         drawKey(mc, "A", Keyboard.isKeyDown(Keyboard.KEY_A),
-                -(boxSize + gap), boxSize + gap, boxSize, bg, textColor);
+                -(box + gap), box + gap, box, bg, textColor);
 
         // S
         drawKey(mc, "S", Keyboard.isKeyDown(Keyboard.KEY_S),
-                0, boxSize + gap, boxSize, bg, textColor);
+                0, box + gap, box, bg, textColor);
 
         // D
         drawKey(mc, "D", Keyboard.isKeyDown(Keyboard.KEY_D),
-                boxSize + gap, boxSize + gap, boxSize, bg, textColor);
+                box + gap, box + gap, box, bg, textColor);
 
         // LMB
-        boolean lmb = org.lwjgl.input.Mouse.isButtonDown(0);
-        drawKeyWide(mc, "LMB", lmb, -(boxSize + gap), (boxSize + gap) * 2,
-                boxSize * 2 + gap, bg, textColor);
+        boolean lmb = Mouse.isButtonDown(0);
+        drawKeyWide(mc, "LMB", lmb, -(box + gap), (box + gap) * 2,
+                box * 2 + gap, bg, textColor);
 
         // RMB
-        boolean rmb = org.lwjgl.input.Mouse.isButtonDown(1);
-        drawKeyWide(mc, "RMB", rmb, boxSize / 2 + gap,
-                (boxSize + gap) * 2, boxSize * 2 + gap, bg, textColor);
+        boolean rmb = Mouse.isButtonDown(1);
+        drawKeyWide(mc, "RMB", rmb, box / 2 + gap,
+                (box + gap) * 2, box * 2 + gap, bg, textColor);
 
         GlStateManager.popMatrix();
     }
@@ -58,7 +60,7 @@ public class HudKeystrokes extends HudModule {
         int textCol = pressed ? 0xFF000000 : textColor;
 
         if (hasBackground() || pressed) {
-            drawRect(x, y, x + size, y + size, color);
+            Gui.drawRect(x, y, x + size, y + size, color);
         }
 
         mc.fontRendererObj.drawString(label,
@@ -74,7 +76,7 @@ public class HudKeystrokes extends HudModule {
         int textCol = pressed ? 0xFF000000 : textColor;
 
         if (hasBackground() || pressed) {
-            drawRect(x, y, x + width, y + height, color);
+            Gui.drawRect(x, y, x + width, y + height, color);
         }
 
         mc.fontRendererObj.drawString(label,
@@ -82,4 +84,4 @@ public class HudKeystrokes extends HudModule {
                 y + (height - mc.fontRendererObj.FONT_HEIGHT) / 2,
                 textCol);
     }
-                                      }
+}
