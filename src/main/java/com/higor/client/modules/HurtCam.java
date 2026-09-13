395e5f77fda453348341f1c81b1fe2e44f4fa987
higor525 <gomeshigor033@gmail.com>
2026-09-13 10:08:51 -0300

    Atualizar o HurtCam.java

--- src/main/java/com/higor/client/modules/HurtCam.java
+++ src/main/java/com/higor/client/modules/HurtCam.java
@@ -5,34 +5,43 @@ import com.higor.client.core.Module;
 import com.higor.client.core.Setting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
-
 import java.lang.reflect.Field;
 
 public class HurtCam extends Module {
-
     private Field hurtCameraField = null;
-    private Field rendererUpdateCountField = null;
     private boolean fieldChecked = false;
-    private int checkCounter = 0;
 
     public HurtCam() {
         super("HurtCam", Category.RENDER);
         addSetting(new Setting("Intensidade", 0.0, 0.0, 2.0, 0.1));
     }
 
-    @Override
-    public void onEnable() {
-        System.out.println("[HIGOR CLIENT] HurtCam ativado");
-    }
-
-    @Override
-    public void onDisable() {
-        System.out.println("[HIGOR CLIENT] HurtCam desativado");
-    }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
 
     @Override
     public void onUpdate() {
         if (!isEnabled()) return;
-
         Minecraft mc = Minecraft.getMinecraft();
         if (mc.thePlayer == null || mc.entityRenderer == null) return;
+        if (!fieldChecked) { findField(); fieldChecked = true; }
+        if (hurtCameraField == null) return;
+        try {
+            if (mc.thePlayer.hurtTime > 0) {
+                float v = (float) getNumber("Intensidade");
+                hurtCameraField.setFloat(mc.entityRenderer, v);
+            }
+        } catch (Exception ignored) {}
+    }
+
+    private void findField() {
+        for (String n : new String[]{"hurtCameraEffect","field_78498_aX"}) {
+            try {
+                Field f = EntityRenderer.class.getDeclaredField(n);
+                f.setAccessible(true);
+                hurtCameraField = f;
+                return;
+            } catch (NoSuchFieldException ignored) {}
+        }
+    }
+}
